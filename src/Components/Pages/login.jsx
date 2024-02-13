c7e9aac69ac06c0b59e25602916f2211521c57cc
dusengepeggy <dickinepeggy100@gmail.com>
2024-02-13 20:05:52 +0200

    login

--- src/Components/Pages/login.jsx
+++ src/Components/Pages/login.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import "./Login.css";
+import "./login.css";
 import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import axios from "axios";
